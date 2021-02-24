293e3bba5da094c347d1f31466f27b52f6eaeb0a
Nishchay Suteri <nishchay271095@gmail.com>
2021-02-24 19:58:11 +0530

    feat: Setup Basic Redux Boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
