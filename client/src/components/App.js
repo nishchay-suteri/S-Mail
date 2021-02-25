35407400216572f5ddb3b016eecdd7bdf6f20bbb
Nishchay Suteri <nishchay271095@gmail.com>
2021-02-25 19:51:18 +0530

    feat: Setup React Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,27 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-    return <div>Hi There!</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                {/* BrowserRouter only expects atmost 1 children */}
+                <div>
+                    {/* Always visible route */}
+                    <Header />
+                    {/* exact={true} and exact both are same */}
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 export default App;
